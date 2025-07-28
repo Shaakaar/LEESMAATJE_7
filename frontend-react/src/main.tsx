f6d04da1a2d52e18c231370313e114c8b302bb8d
Shaakaar <shakaarlatief@hotmail.com>
2025-07-28 19:14:19 +0200

    Revert "Merge pull request #109 from Shaakaar/codex/build-new-learner-dashboard-experience"
    This reverts commit 322933bd22cdcefcc62a7bff300e08c82707cc44, reversing
    changes made to 84fc41928a06598e059d91d982270279a9d93588.

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
+import LoginPage from './pages/LoginPage';
 import { ModelInitProvider } from './lib/ModelInitContext';
-import AppRouter from './router/AppRouter';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ModelInitProvider>
-      <AppRouter />
-    </ModelInitProvider>
+    <BrowserRouter>
+      <ModelInitProvider>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </ModelInitProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
