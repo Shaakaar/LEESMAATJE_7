6194d439a67a52bc5dfbd3d394de7f792774bf66
Shaakaar <shakaarlatief@hotmail.com>
2025-07-29 13:42:18 +0200

    feat(frontend): show textual feedback

--- frontend-react/src/components/story/Word.tsx
+++ frontend-react/src/components/story/Word.tsx
@@ -1,15 +1,16 @@
 interface Props {
   children: string;
   audio?: string;
+  wrong?: boolean;
 }
 
-export default function Word({ children, audio }: Props) {
+export default function Word({ children, audio, wrong }: Props) {
   function play() {
     if (audio) new Audio('/api/audio/' + audio).play();
   }
   return (
     <span
-      className="cursor-pointer hover:text-primary"
+      className={`word hover:text-primary ${wrong ? 'wrong' : ''}`}
       onClick={play}
     >
       {children}{' '}
