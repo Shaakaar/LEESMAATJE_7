6e22a43387ae0796b287981469c1023e07836f08
Shaakaar <shakaarlatief@hotmail.com>
2025-07-30 19:14:20 +0200

    Replace ScriptProcessorNode with AudioWorkletNode

---
+++ frontend-react/src/worklets/pcm-processor.ts
@@ -0,0 +1,18 @@
+class PCMProcessor extends AudioWorkletProcessor {
+  process(inputs: Float32Array[][]) {
+    const input = inputs[0];
+    if (input && input[0]) {
+      const samples = input[0];
+      const pcm = new Int16Array(samples.length);
+      for (let i = 0; i < samples.length; i++) {
+        const s = Math.max(-1, Math.min(1, samples[i]));
+        pcm[i] = s * 32767;
+      }
+      this.port.postMessage(pcm);
+    }
+    return true;
+  }
+}
+
+registerProcessor('pcm-processor', PCMProcessor);
+export {};
