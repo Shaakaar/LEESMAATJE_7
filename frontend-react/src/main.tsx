74855ee23ae1410ed5fbcd727fee3d0ecda1b0e1
Shaakaar <shakaarlatief@hotmail.com>
2025-08-05 15:59:47 +0200

    feat: show loading page for story directions

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -10,6 +10,7 @@ import PlayPage from './pages/PlayPage';
 import ProgressPage from './pages/ProgressPage';
 import RequireAuth from './components/RequireAuth';
 import StoryPage from './pages/StoryPage';
+import ContinuePage from './pages/ContinuePage';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -22,6 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             <Route path="/dashboard/level/:levelId" element={<LevelPage />} />
             <Route path="/play/:levelId/:themeId" element={<PlayPage />} />
             <Route path="/story" element={<StoryPage />} />
+            <Route path="/continue" element={<ContinuePage />} />
             <Route path="/progress" element={<ProgressPage />} />
           </Route>
           <Route path="*" element={<Navigate to="/" replace />} />
